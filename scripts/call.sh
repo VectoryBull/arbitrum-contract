25ad0e386ce7721e4c1ef8cb1869e6250365fbbf
Alberto Zurini <alberto.zurini@gmail.com>
2025-04-03 17:44:18 +0300

    wip: compiler error to be fixed, just keeping this for tracking

--- scripts/call.sh
+++ scripts/call.sh
@@ -1,4 +1,13 @@
-cast call --rpc-url 'http://localhost:8547' --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
-0x4a2ba922052ba54e29c5417bc979daaf7d5fe4f4 "temperature()(int256)"
+CONTRACT=0x408da76e87511429485c32e4ad647dd14823fdc4
 
-cast storage 0x4a2ba922052ba54e29c5417bc979daaf7d5fe4f4 0 --rpc-url 'http://localhost:8547'
+cast send --rpc-url 'http://localhost:8547' --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
+$CONTRACT "init(int256,int256,int256,int256,int256)" 1 2 3 4 5 6
+
+cast send --rpc-url 'http://localhost:8547' --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
+$CONTRACT "storeSensorData(int256,int256,int256,int256,int256)" 1 2 3 4 5
+
+cast call --rpc-url 'http://localhost:8547' \
+$CONTRACT "printTemp(uint256)(int256)" 0 \
+--private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659 \
+
+cast storage $CONTRACT 0 --rpc-url 'http://localhost:8547'
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
 extern crate alloc;
 
@@ -9,36 +10,45 @@ use stylus_sdk::{alloy_primitives::*, prelude::*, storage::*};
 #[storage]
 #[entrypoint]
 pub struct IoTDataProcessor {
-    temperature: StorageVec<StorageI256>,
-    humidity: StorageVec<StorageI256>,
-    vibration1: StorageVec<StorageI256>,
-    vibration2: StorageVec<StorageI256>,
-    gyro: StorageVec<StorageI256>,
+    temperature: StorageVec<StorageU256>,
+    humidity: StorageVec<StorageU256>,
+    vibration1: StorageVec<StorageU256>,
+    vibration2: StorageVec<StorageU256>,
+    gyro: StorageVec<StorageU256>,
+
+    min_temp: StorageU256,
+    max_temp: StorageU256,
+    min_hum: StorageU256,
+    max_hum: StorageU256,
+    max_vib1: StorageU256,
+    max_vib2: StorageU256,
+
+    FINAL_STATE: StorageU256, // 0: Needs to be checked, 1: Okay, 2: Not okay
 }
 
 
 /// Declare that `Counter` is a contract with the following external methods.
 #[public]
 impl IoTDataProcessor {
-    pub fn constructor(&self,
-        min_temp: I256,
-        max_temp: I256,
-        min_hum: I256,
-        max_hum: I256,
-        max_vib1: I256,
-        max_vib2: I256 
-        // TODO: public key
+    pub fn init(&mut self,
+        min_temp: U256,
+        max_temp: U256,
+        min_hum: U256,
+        max_hum: U256,
+        max_vib1: U256,
+        max_vib2: U256 
         ) {
-        //
+        let tmp = U256::from(0);
+        self.FINAL_STATE.set(tmp);
     }
 
     pub fn store_sensor_data(
         &mut self,
-        temperature: I256,
-        humidity: I256,
-        vibration1: I256,
-        vibration2: I256,
-        gyro: I256,
+        temperature: U256,
+        humidity: U256,
+        vibration1: U256,
+        vibration2: U256,
+        gyro: U256,
     ) {
         self.temperature.push(temperature);
         self.humidity.push(humidity);
@@ -47,39 +57,84 @@ impl IoTDataProcessor {
         self.gyro.push(gyro);
     }
 
-    /*
-
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+    fn is_temperature_within_bounds(&self, temperature: U256) -> bool {
+        temperature >= self.min_temp.get() && 
+        temperature <= self.max_temp.get()
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
+    fn is_humidity_within_bounds(&self, humidity: U256) -> bool {
+        humidity >= self.min_hum.get() && 
+        humidity <= self.max_hum.get()
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
+    fn is_vibration1_within_bounds(&self, vibration1: U256) -> bool {
+        vibration1 <= self.max_vib1.get()
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
+    fn is_vibration2_within_bounds(&self, vibration2: U256) -> bool {
+        vibration2 <= self.max_vib2.get()
     }
 
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+    pub fn terminate_data_collection(
+        &mut self
+    ) {
+        let mut all_within_bounds = true;
+
+        for i in 0..self.temperature.len() {
+            let temp = self.temperature.get(i).unwrap();
+            if !self.is_temperature_within_bounds(temp) {
+            all_within_bounds = false;
+            break;
+            }
+        }
+
+        if all_within_bounds {
+            for i in 0..self.humidity.len() {
+            let hum = self.humidity.get(i).unwrap();
+            if !self.is_humidity_within_bounds(hum) {
+                all_within_bounds = false;
+                break;
+            }
+            }
+        }
+
+        if all_within_bounds {
+            for i in 0..self.vibration1.len() {
+            let vib1 = self.vibration1.get(i).unwrap();
+            if !self.is_vibration1_within_bounds(vib1) {
+                all_within_bounds = false;
+                break;
+            }
+            }
+        }
+
+        if all_within_bounds {
+            for i in 0..self.vibration2.len() {
+            let vib2 = self.vibration2.get(i).unwrap();
+            if !self.is_vibration2_within_bounds(vib2) {
+                all_within_bounds = false;
+                break;
+            }
+            }
+        }
+
+        if all_within_bounds {
+            // All data is within bounds, proceed with termination logic
+            let tmp = U256::from(1);
+            self.FINAL_STATE.set(tmp); // Okay
+        } else {
+            // Handle the case where some data is out of bounds
+            let tmp = U256::from(2);
+            self.FINAL_STATE.set(tmp); // Not okay
+        }
     }
 
-    /// Adds the wei value from msg_value to the number in storage.
-    #[payable]
-    pub fn add_from_msg_value(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + self.vm().msg_value());
+    pub fn print_temp(&mut self,
+    i: U256) -> Signed<256, 4> {
+        self.temperature.get(i).unwrap()
     }
-        */
+
+    pub fn print_final_state(&mut self) -> U256 {
+        self.FINAL_STATE.get()
+    }
 }
